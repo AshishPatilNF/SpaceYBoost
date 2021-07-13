a74286603b979e29bfe2ff5e4def5a072f20e30a
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-07-13 21:14:13 +0530

    Implemented PlayerRocket movement using AddRelativeForce

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float upThrust = 1000;
+
+    [SerializeField]
+    float rotateThrust = 100;
+
+    Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(upThrust * Time.deltaTime * Vector3.up);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(rotateThrust * Time.deltaTime * Vector3.forward);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(rotateThrust * Time.deltaTime * -Vector3.forward);
+        }
+
+        //transform.Rotate(0, 0, Input.GetAxis("Horizontal") * -rotateThrust * Time.deltaTime);
+    }
+}
