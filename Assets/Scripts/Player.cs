f33bd24d5e171d048ea9d588b2b81ceef0a7d4f3
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-07-15 14:18:15 +0530

    Added Rocket launch SFX and new Game level

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -11,12 +12,18 @@ public class Player : MonoBehaviour
     [SerializeField]
     float rotateThrust = 100;
 
+    int currentSceneIndex;
+
     Rigidbody rigidBody;
+
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
     }
 
     // Update is called once per frame
@@ -29,9 +36,17 @@ public class Player : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            if (!audioSource.isPlaying)
+                audioSource.Play();
+
             rigidBody.AddRelativeForce(upThrust * Time.deltaTime * Vector3.up);
         }
-
+        else
+        {
+            if (audioSource.isPlaying)
+                audioSource.Stop();
+        }
+        
         if (Input.GetKey(KeyCode.A))
         {
             rigidBody.freezeRotation = true;
@@ -46,5 +61,25 @@ public class Player : MonoBehaviour
         rigidBody.freezeRotation = false;
 
         //transform.Rotate(0, 0, Input.GetAxis("Horizontal") * -rotateThrust * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "LaunchPad":
+                break;
+            case "Finish":
+                int nextScene = currentSceneIndex + 1;
+                if (currentSceneIndex + 1 == SceneManager.sceneCountInBuildSettings)
+                    nextScene = 0;
+                SceneManager.LoadScene(nextScene);
+                break;
+            case "Fuel":
+                break;
+            default:
+                SceneManager.LoadScene(currentSceneIndex);
+                break;
+        }
     }
 }
