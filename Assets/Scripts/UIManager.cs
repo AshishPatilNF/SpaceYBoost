f13573b2f7c698dbc7469d0b75b2fbe4dffcb6ad
Ashish Patil <ashishsanjaypatil@gmail.com>
2021-07-27 20:48:08 +0530

    Added Start and End Screens

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class UIManager : MonoBehaviour
@@ -10,6 +11,21 @@ public class UIManager : MonoBehaviour
 
     public void UpdateFuel(int fuel)
     {
-        text.text = "Fuel: " + fuel;
+        text.text = "" + fuel;
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitApp()
+    {
+        Application.Quit();
     }
 }
